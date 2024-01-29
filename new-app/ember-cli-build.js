a4496deb1e7eaf736881ada7ba33abe2657d4782
Ryan Tablada <ryan.tablada@gmail.com>
2024-01-29 17:54:03 -0600

    feat: try to configure publicAssetURL

--- new-app/ember-cli-build.js
+++ new-app/ember-cli-build.js
@@ -5,12 +5,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    "ember-cli-babel": {
-      enableTypeScriptTransform: true
-    }
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+    },
   });
 
   return require('@embroider/compat').compatBuild(app, Webpack, {
-
+    packagerOptions: {
+      publicAssetURL: EmberApp.env() === 'development' ? '/new-app-assets/' : '/', // This should be a URL ending in "/"
+    }
   });
 };
